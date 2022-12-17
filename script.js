e2b4a61a5d627d7d8d74975c00f1f5a0cd902bce
maruli9 <118851819+maruli9@users.noreply.github.com>
2022-12-17 13:24:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const listContainer = document.getElementsByClassName('list-container')[0]
+
+toggleButton.addEventListener('click', ()=>{
+    listContainer.classList.toggle('active')
+})
+
+function back(){
+    window.history.back()
+}
